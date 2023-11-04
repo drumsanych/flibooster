d4f83a2a29a36f78eea480e7ac00526ac8548a07
alexartrf <alex.art.rf@yandex.ru>
2023-11-04 08:30:06 +0300

    initial commit

---
+++ src/test/java/net/yokniga/yoik/YoikApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yokniga.yoik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
